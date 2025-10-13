a3a93d1efd29f622ba73980c681c75bff9f0bc93
sherin-dev <sherinshanavas123@gmail.com>
2025-10-13 12:04:39 +0530

    adding the homecategory page to the branch sherin

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Banner from "./Banner";
+import HomeCategory from "./HomeCategory";
 const Home=()=>{
     return(
         <>
         <Banner/>
+        <HomeCategory/>
         </>
 
     )
