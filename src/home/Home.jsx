0e525a80ccfe06b5a12dfc6b11c26a80fa7f4a61
sherin-dev <sherinshanavas123@gmail.com>
2025-10-14 14:01:26 +0530

    adding the Home page to the develop branch online reposiroty

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "./Banner";
 import HomeCategory from "./HomeCategory";
 import CategoryShowcase from "./CategoryShowcase";
+import LocationSprade from "./LocationSprade";
 const Home=()=>{
     return(
         <>
         <Banner/>
         <HomeCategory/>
-        <CategoryShowcase/>
+        <CategoryShowcase/> 
+        <LocationSprade/>       
         </>
     )
 }
