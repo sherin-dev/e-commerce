e2dc3e5dc7ed1184b27d93126eb994e5bffb9251
souparnikarb <souparnikarb714@gmail.com>
2025-10-15 14:29:28 +0530

    adding blog componente and shop

---
+++ src/assets/shop/Pagination.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+const Pagination = ({productsPerPage, totalProducts, Paginate, activePage}) => {
+    const pageNumbers =[]
+    return(
+        <div>Pagination</div>
+    )
+}
+export default Pagination
